f213a12b3e75239f872fb8ce8d571fa12a0c9020
morshed0099 <mdgolammorshed0099@gmail.com>
2023-08-14 11:58:52 +0600

    large screen navbar create

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
-            this is navbar
+            <nav className='container  mx-auto'>
+                <div className='hidden lg:flex justify-between items-center '>
+                    <div>
+                        <h2 className='text-2xl font-bold text-green-600'>Job Protal</h2>
+                    </div>
+                    <div className='flex flex-row gap-4'>
+                        <NavLink to='/'>Home</NavLink>
+                        <NavLink to='/about'>About Us</NavLink>
+                        <NavLink to='/contact '>Contact Us</NavLink>
+                        <NavLink to='/signin'>SignIn</NavLink>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,16 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home";
+import About from '../pages/About'
+import Contact from '../pages/Contact'
+import SignIn from "../pages/SignIn";
 
-export const router=createBrowserRouter([
+export const router = createBrowserRouter([
     {
-        path:'/',
-        element:<Main />,
-        children:[
+        path: '/',
+        element: <Main />,
+        children: [
             {
-                path:'/',
-                element:<Home />
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/contact',
+                element: <Contact />
+            },
+            {
+                path: '/signin',
+                element: <SignIn />
             }
-        ]        
+        ]
     }
 ])
